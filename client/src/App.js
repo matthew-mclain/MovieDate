e721e73ab35f16a44e77e9f2c7f48cfe5c09a346
Matthew McLain <mwm.mclain@gmail.com>
2024-04-05 21:44:31 -0400

    Changed app title icon (favicon)

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import popcorn from './components/icons/popcorn.svg';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
@@ -8,6 +9,20 @@ import Dashboard from './components/Dashboard';
 import Profile from './components/Profile';
 
 function App() {
+  useEffect(() => {
+    document.title = 'MovieDate';
+
+    // Change the favicon by setting the href of the existing link element in the head
+    const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.rel = 'icon';
+    link.href = popcorn;
+    document.head.appendChild(link);
+
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, []);
+
   return (
     <Router>
       <Routes>
